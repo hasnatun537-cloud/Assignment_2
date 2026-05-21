248bb34b3965efe6bb5ed34da720eb3426bf30cd
hasnatun537-cloud <hasnatun537@gmail.com>
2026-05-21 23:51:40 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,93 +1,87 @@
-<?php
-include "header.php";
+<?php 
+session_start();
+  include "header.php";
+  
+
+ ?>
+ <div class="container">
+    <?php
+    if(isset($_SESSION['success'])){?>
+        <div class="toast show" role="alert" aria-live="assertive" aria-atomic="true">
+  <div class="toast-header">
+    
+    <strong class="me-auto text-success">Successfully user added!</strong>
+    
+    <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+  </div>
+  <div class="toast-body text-success">
+    <?=$_SESSION['success']?>
+  </div>
+</div>
+    <?php  }
+    ?>
+   <div class="card col-md-6 mx-auto mt-3">
+    <div class="card-header">
+        <h3 class="text-center text-secondary">Add User</h3>
+    </div>
+    <div class="card-body">
+        <form action="./controllers/addUserController.php" method="POST" enctype="multipart/form-data">
+    <div class="mb-3">
+        <label for="name" class="form-label">Name</label>
+        <input type="text" name="name" id="name" class="form-control" placeholder="Enter your name" />
+    <?php
+if(isset($_SESSION['name_err'])){?>
+    <span class="text-danger"><?= $_SESSION['name_err'] ?> </span>
+
+<?php  }
 ?>
-<div class="container">
+    </div>
+    <div class="mb-3">
+        <label for="email" class="form-label">Email</label>
+        <input type="email" name="email" id="email" class="form-control" placeholder="Enter your email" />
+      <?php
+if(isset($_SESSION['email_err'])){?>
+    <span class="text-danger"><?= $_SESSION['email_err'] ?> </span>
 
-<div class="card col-md-6 mx-auto mt-3">
+<?php  }
+?>
+    </div>
+    
+    <div class="mb-3">
+        <label for="description" class="from-label">Description</label>
+        <textarea name="description"  class="form-control summernote"></textarea>
+        <?php
+if(isset($_SESSION['description_err'])){?>
+    <span class="text-danger"><?= $_SESSION['description_err'] ?> </span>
 
-<div class="card-header">
-
-<h3 class="text-center text-secondary">Add User</h3>
-
-</div>
-
-
-<div class="card-body">
-
-<form action="" method="post">
-
-<div class="mb-3">
-
-<label for="name" class="form-Label">Name</Label>
-
-<input type="text" name="name" id="name" class="form-control" placeholder="Enter your name" />
-
-</div>
-
-<div class="mb-3">
-
-<label for="email" class="form-Label">Email</label>
-
-<input type="email" name="email" id="email" class="form-control" placeholder="Enter your email" />
-
-</div>
-
-
-<div class="mb-3">
-
-<label for="phone" class="form-Label">Phone</label>
-
-<input type="text" name="phone" id="phone" class="form-control" placeholder="Enter your phone number" />
-
-</div>
-
-<div class="mb-3">
-
-<label for="description" class="form-Label">Description</label>
-
-<textarea name="description" id="summernote" class="form-control"></textarea>
-
-</div>
-<div class="mb-3">
-
-<label for="experience" class="form-Label">Experience</label>
-
-<textarea name="experience" id="" class="form-control"></textarea>
-
-</div>
-<div class="mb-3">
-
-<label for="project" class="form-Label">Project</label>
-
-<textarea name="project" id="" class="form-control"></textarea>
-
-</div>
-
-<button type="submit" name="submit" class="btn btn-primary w-100">Submit</button>
-
+<?php  }
+?>
+    </div>
+    <div class="mb-3">
+        <label for="Experience" class="from-label">Experience</label>
+        <textarea name="Experience" id="" class="form-control summernote"></textarea>
+    </div>
+    <div class="mb-3">
+        <label for="Project" class="from-label">Project</label>
+        <textarea name="Project" id="" class="form-control summernote"></textarea>
+    </div>
+    <div class="mb-3">
+        <label for="Profile" class="from-label">Profile</label>
+        <input type="file" name="profile_image" class="form-control">
+    </div>
+    <button type="submit" name="submit" class="btn btn-primary w-100">Submit</button>
 </form>
-
-</div>
-
-
-</div>
-
-
-</div>
+    </div>
+   </div>
+  </div>
 <?php
-include "footer.php";
-
+    include "footer.php";
+    session_unset();
 ?>
-    <script>
-      $('#summernote').summernote({
-        placeholder: 'Hello Bootstrap 5',
-        tabsize: 2,
-        height: 100
-      });
-    </script>
-
-
-
-
-
-
+<script>
+    $(document).ready(function() {
+        $('.summernote').summernote({
+            height: 100
+        });
+    });
+</script>
